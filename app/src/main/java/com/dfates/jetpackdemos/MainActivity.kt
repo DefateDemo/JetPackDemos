3135df44185499d0507c7a231fb0d6814207218e
Gong Bo <1326114523@qq.com>
2018-12-08 16:51:06 +0800

    gongbo

--- app/src/main/java/com/dfates/jetpackdemos/MainActivity.kt
+++ app/src/main/java/com/dfates/jetpackdemos/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import com.dfates.jetpackdemos.R
+import com.dfates.jetpackdemos.liveData.LiveDataActivity
 import com.dfates.jetpackdemos.room.RoomActivity
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -84,7 +85,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             }
             R.id.livedata -> {
-
+                startActivity(Intent(this, LiveDataActivity::class.java))
             }
             R.id.navigation -> {
 
--- app/src/main/java/com/dfates/jetpackdemos/liveData/LiveDataActivity.kt
+++ app/src/main/java/com/dfates/jetpackdemos/liveData/LiveDataActivity.kt
@@ -1,33 +1,47 @@
 package com.dfates.jetpackdemos.liveData
 
-import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import com.dfates.jetpackdemos.R
 
 class LiveDataActivity : AppCompatActivity() {
 
     private lateinit var mModel: NameViewModel
 
+    private lateinit var tvText: TextView
+    private lateinit var btnModify: Button
+
+    private var value = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_live_data)
 
+
+        tvText = findViewById(R.id.tv_text)
+        btnModify = findViewById(R.id.btn_modify)
+
         // Get the ViewModel.
         mModel = ViewModelProviders.of(this).get(NameViewModel::class.java)
-
 
         // Create the observer which updates the UI.
         val nameObserver = Observer<String> { newName ->
             // Update the UI, in this case, a TextView.
-            mNameTextView.text = newName
+            tvText.text = newName
         }
 
         // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
         mModel.currentName.observe(this, nameObserver)
+
+        btnModify.setOnClickListener { v ->
+            mModel.currentName.value = value.toString() + ""
+            value++
+        }
     }
 
 }
