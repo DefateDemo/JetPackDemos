ef478f197b4a03ffbd7b2f3b7abc0bd08f7fca41
Gong Bo <1326114523@qq.com>
2018-12-19 15:44:33 +0800

    gong.bo

--- app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonAdapter.kt
+++ app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonAdapter.kt
@@ -9,22 +9,27 @@ import com.dfates.jetpackdemos.common.toSparseIntArray
 
 //数据-视图转换函数别名
 typealias CommonAdapterConvert<M> = (holder: SimpleViewHolder, data: M?, position: Int) -> Unit
+
 /**
  * ListView,GridView简洁适配器，使用传入闭包处理的方式实现
  */
-open class CommonAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, var convert: CommonAdapterConvert<M>?) : BaseSimpleAdapter<M, SimpleViewHolder>(mContext, layoutIds, mDatas) {
+open class CommonAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, var convert: CommonAdapterConvert<M>? = null) : BaseSimpleAdapter<M, SimpleViewHolder>(mContext, layoutIds, mDatas) {
 
     /**
      * 使用arrayOf(viewType to layoutId,...) 的形式传入多个布局
      */
-    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: CommonAdapterConvert<M>?) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
+    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: CommonAdapterConvert<M>? = null) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
 
     /**
      * 传入单个布局
      */
-    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: CommonAdapterConvert<M>?) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
+    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: CommonAdapterConvert<M>? = null) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
 
     override fun setView(convertView: View, holder: SimpleViewHolder, data: M?, position: Int) {
+        convert(holder, data, position)
+    }
+
+    open fun convert(holder: SimpleViewHolder, data: M?, position: Int) {
         convert?.invoke(holder, data, position)
     }
 
--- app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonRecycleViewAdapter.kt
+++ app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonRecycleViewAdapter.kt
@@ -10,22 +10,27 @@ import com.dfates.jetpackdemos.common.toSparseIntArray
 
 ////数据-视图转换函数别名
 typealias CommonRecycleViewAdapterConvert<M> = ((holder: RecycleViewHolder, data: M?, position: Int, viewType: Int) -> Unit)
+
 /**
  * RecycleView简洁适配器，，使用传入闭包处理的方式实现
  */
-open class CommonRecycleViewAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, private var convert: CommonRecycleViewAdapterConvert<M>?) : BaseRecycleViewAdapter<M>(mContext, layoutIds, mDatas) {
+open class CommonRecycleViewAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, private var convert: CommonRecycleViewAdapterConvert<M>? = null) : BaseRecycleViewAdapter<M>(mContext, layoutIds, mDatas) {
     /**
      * 使用arrayOf(viewType to layoutId,...) 的形式传入多个布局
      */
-    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>?) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
+    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>? = null) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
 
     /**
      * 传入单个布局
      */
-    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>?) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
+    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>? = null) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
 
 
     override fun onBindViewHolder(holder: RecycleViewHolder, data: M?, position: Int, viewType: Int) {
+        convert(holder, data, position, viewType)
+    }
+
+    open fun convert(holder: RecycleViewHolder, data: M?, position: Int, viewType: Int) {
         convert?.invoke(holder, data, position, viewType)
     }
 
