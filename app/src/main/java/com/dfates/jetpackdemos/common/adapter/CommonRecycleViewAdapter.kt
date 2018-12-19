b5d7f7b170986ec3639c508d9cff1383b376714b
Gong Bo <1326114523@qq.com>
2018-12-19 11:47:57 +0800

    gong.bo

--- app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonRecycleViewAdapter.kt
+++ app/src/main/java/com/dfates/jetpackdemos/common/adapter/CommonRecycleViewAdapter.kt
@@ -8,19 +8,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dfates.jetpackdemos.base.adapter.BaseRecycleViewAdapter
 import com.dfates.jetpackdemos.common.toSparseIntArray
 
+////数据-视图转换函数别名
+typealias CommonRecycleViewAdapterConvert<M> = ((holder: RecycleViewHolder, data: M?, position: Int, viewType: Int) -> Unit)
 /**
  * RecycleView简洁适配器，，使用传入闭包处理的方式实现
  */
-open class CommonRecycleViewAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, var convert: ((RecycleViewHolder, M?, position: Int, viewType: Int) -> Unit)?) : BaseRecycleViewAdapter<M>(mContext, layoutIds, mDatas) {
+open class CommonRecycleViewAdapter<M>(mContext: Context, layoutIds: SparseIntArray?, mDatas: List<M?>?, private var convert: CommonRecycleViewAdapterConvert<M>?) : BaseRecycleViewAdapter<M>(mContext, layoutIds, mDatas) {
     /**
      * 使用arrayOf(viewType to layoutId,...) 的形式传入多个布局
      */
-    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: ((RecycleViewHolder, M?, position: Int, viewType: Int) -> Unit)?) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
+    constructor(mContext: Context, layoutIds: Array<Pair<Int, Int>>, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>?) : this(mContext, layoutIds.toSparseIntArray(), mDatas, convert)
 
     /**
      * 传入单个布局
      */
-    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: ((RecycleViewHolder, M?, position: Int, viewType: Int) -> Unit)?) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
+    constructor(mContext: Context, layoutId: Int, mDatas: List<M?>?, convert: CommonRecycleViewAdapterConvert<M>?) : this(mContext, arrayOf(0 to layoutId), mDatas, convert)
 
 
     override fun onBindViewHolder(holder: RecycleViewHolder, data: M?, position: Int, viewType: Int) {
