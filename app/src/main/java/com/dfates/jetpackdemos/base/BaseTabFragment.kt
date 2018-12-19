772505494ca22645bc4d18d99177cf74fc9bbfcb
Gong Bo <1326114523@qq.com>
2018-12-19 11:19:59 +0800

    gong.bo

--- app/src/main/java/com/dfates/jetpackdemos/base/BaseTabFragment.kt
+++ app/src/main/java/com/dfates/jetpackdemos/base/BaseTabFragment.kt
@@ -1,24 +1,18 @@
 package com.dfates.jetpackdemos.base
 
-import androidx.databinding.ViewDataBinding
 import androidx.viewpager.widget.ViewPager
-import com.dfates.jetpackdemos.common.findViewById
 import com.google.android.material.tabs.TabLayout
 
 /**
  * Created by $USER_NAME on 2018/12/13.
  */
-abstract class BaseTabFragment<VB : ViewDataBinding>(layoutId: Int, val tabLayoutId: Int, val viewPagerId: Int) : BaseBindingFragment<VB>(layoutId), ITabFragment {
+abstract class BaseTabFragment(layoutId: Int) : BaseFragment(layoutId), ITabFragment {
 
     lateinit var tabLayout: TabLayout
     lateinit var viewPager: ViewPager
 
     override fun initView() {
         super.initView()
-
-        tabLayout = findViewById(tabLayoutId)!!
-        viewPager = findViewById(viewPagerId)!!
-
         initTab(context!!, tabLayout, viewPager, childFragmentManager)
     }
 
