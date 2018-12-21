2650da179c653d448d76c3c92d2bd68118d512d2
Gong Bo <1326114523@qq.com>
2018-12-21 10:56:19 +0800

    gong.bo

--- app/src/main/java/com/dfates/jetpackdemos/viewModel/ParentViewModelFragment.kt
+++ app/src/main/java/com/dfates/jetpackdemos/viewModel/ParentViewModelFragment.kt
@@ -6,10 +6,11 @@ import androidx.viewpager.widget.ViewPager
 import com.dfates.jetpackdemos.R
 import com.dfates.jetpackdemos.base.databinding.BaseBindingTabFragment
 import com.dfates.jetpackdemos.common.bind.BindView
+import com.dfates.jetpackdemos.databinding.FragmentParentViewModelBinding
 import com.google.android.material.tabs.TabLayout
 import java.util.*
 
-class ParentViewModelFragment : BaseBindingTabFragment<com.dfates.jetpackdemos.databinding.FragmentParentViewModelBinding>(R.layout.fragment_parent_view_model) {
+class ParentViewModelFragment : BaseBindingTabFragment<FragmentParentViewModelBinding>(R.layout.fragment_parent_view_model) {
 
     override val pages: List<Pair<String, Fragment>> by lazy {
         Arrays.asList("TAB1" to ViewModelFragment(), "TAB2" to ViewModelFragment(), "TAB3" to ViewModelFragment())
